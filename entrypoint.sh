b176ec216959cf61ab84f69348ac56e12d3d780d
Aayushi-jain22 <91220758+Aayushi-jain22@users.noreply.github.com>
2026-07-15 17:38:36 +0530

    Upated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,4 +21,5 @@ for meal in meals:
 print('Seed sync completed')
 "
 
-gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+# gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --timeout 120
